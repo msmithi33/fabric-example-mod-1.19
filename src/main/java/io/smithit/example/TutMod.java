9d3ea50d7601dd46f0ef7b10cc7b6ddc140683b8
msmithi33 <msmithi33@gmail.com>
2022-11-05 14:45:18 -0700

    Adding Blocks

--- src/main/java/io/smithit/example/TutMod.java
+++ src/main/java/io/smithit/example/TutMod.java
@@ -1,21 +1,17 @@
 package io.smithit.example;
 
+import io.smithit.example.block.ModBlocks;
+import io.smithit.example.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name. Dude.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
+	public static final String MOD_ID = "tutmod";
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/io/smithit/example/item/ModItems.java
+++ src/main/java/io/smithit/example/item/ModItems.java
@@ -1,4 +1,23 @@
 package io.smithit.example.item;
 
+import io.smithit.example.TutMod;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class ModItems {
+
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot", new Item( new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget", new Item( new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item MYTHRIL_RAW = registerItem("mythril_raw", new Item( new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        TutMod.LOGGER.info("Registering Mod Items for " + TutMod.MOD_ID);
+    }
 }
